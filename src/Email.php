c6c3067067ea1a190316154117cf74dc249b3476
Emmanuel Obua <emmanuelo@intanode.co.ug>
2024-05-09 18:20:18 +0300

    email sending

--- src/Email.php
+++ src/Email.php
@@ -1,5 +1,6 @@
 <?php
 namespace UTel\SDK;
+use GuzzleHttp\Exception\GuzzleException;
 
 class Email extends Service
 {
@@ -12,37 +13,43 @@ class Email extends Service
 	public function send($options)
 	{
 
-		if (empty($options['from']) || empty($options['to']) || empty($options['message']) || empty($options['subject'])) {
-			return $this->error('recipient, subject and message must be defined');
+		try {
+
+			if (empty($options['from']) || empty($options['to']) || empty($options['message']) || empty($options['subject'])) {
+				return $this->error('recipient, subject and message must be defined');
+			}
+			
+			$curl = curl_init();
+
+			curl_setopt_array($curl, array(
+				CURLOPT_URL => $this->baseDomain,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => '',
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 0,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => 'POST',
+				CURLOPT_POSTFIELDS =>'{
+					"From": "'.$options['from'].'",
+					"To": "'.$options['to'].'",
+					"Cc": "'.$options['cc'].'",
+					"Sub": "'.$options['subject'].'",
+					"Msg": "'.$options['message'].'",
+				}',
+				CURLOPT_HTTPHEADER => array(
+					'Content-Type: application/json',
+					'Accept: application/json'
+				),
+			));
+
+			$response = curl_exec($curl);
+			
+			curl_close($curl);
+			
+		} catch (\Exception $e) {
+			return $this->error($e->getMessage());
 		}
-		
-		$curl = curl_init();
-
-		curl_setopt_array($curl, array(
-			CURLOPT_URL => $this->baseDomain,
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_ENCODING => '',
-			CURLOPT_MAXREDIRS => 10,
-			CURLOPT_TIMEOUT => 0,
-			CURLOPT_FOLLOWLOCATION => true,
-			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST => 'POST',
-			CURLOPT_POSTFIELDS =>'{
-				"From": "'.$options['from'].'",
-				"To": "'.$options['to'].'",
-				"Cc": "'.$options['cc'].'",
-				"Sub": "'.$options['subject'].'",
-				"Msg": "'.$options['message'].'",
-			}',
-			CURLOPT_HTTPHEADER => array(
-				'Content-Type: application/json',
-				'Accept: application/json'
-			),
-		));
-
-		$response = curl_exec($curl);
-		
-		curl_close($curl);
 
 		return $this->success($response);
 	}
--- src/SMS.php
+++ src/SMS.php
@@ -12,35 +12,41 @@ class SMS extends Service
 	public function send($options)
 	{
 
-		if (empty($options['sender']) || empty($options['to']) || empty($options['message'])) {
-			return $this->error('recipient and message must be defined');
+		try {
+
+			if (empty($options['sender']) || empty($options['to']) || empty($options['message'])) {
+				return $this->error('recipient and message must be defined');
+			}
+			
+			$curl = curl_init();
+
+			curl_setopt_array($curl, array(
+				CURLOPT_URL => $this->baseDomain,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => '',
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 0,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => 'POST',
+				CURLOPT_POSTFIELDS =>'{
+					"sender": "'.$options['sender'].'",
+					"recipient": "'.$options['to'].'",
+					"message": "'.$options['message'].'"
+				}',
+				CURLOPT_HTTPHEADER => array(
+					'Content-Type: application/json',
+					'Authorization: Basic '.$this->token
+				),
+			));
+
+			$response = curl_exec($curl);
+			
+			curl_close($curl);
+			
+		} catch (\Exception $e) {
+			return $this->error($e->getMessage());
 		}
-		
-		$curl = curl_init();
-
-		curl_setopt_array($curl, array(
-			CURLOPT_URL => $this->baseDomain,
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_ENCODING => '',
-			CURLOPT_MAXREDIRS => 10,
-			CURLOPT_TIMEOUT => 0,
-			CURLOPT_FOLLOWLOCATION => true,
-			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST => 'POST',
-			CURLOPT_POSTFIELDS =>'{
-				"sender": "'.$options['sender'].'",
-				"recipient": "'.$options['to'].'",
-				"message": "'.$options['message'].'"
-			}',
-			CURLOPT_HTTPHEADER => array(
-				'Content-Type: application/json',
-				'Authorization: Basic '.$this->token
-			),
-		));
-
-		$response = curl_exec($curl);
-		
-		curl_close($curl);
 
 		return $this->success($response);
 	}
